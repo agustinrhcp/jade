2c9549913852c7fdc5c3ba2ee93595f7aaefffe4
agustin <agustincornu@fastmail.com>
2026-04-20 11:05:21 -0300

    Add formatter.

---
+++ lib/jade/formatter.rb
@@ -0,0 +1,364 @@
+module Jade
+  module Formatter
+    extend self
+
+    INDENT = "  "
+
+    def format(node, indent: 0)
+      case node
+      in AST::Module(name:, exposing:, body:)
+        body
+          .expressions
+          .map { format(it, indent:) }
+          .then { ["module #{name} #{format_exposing(exposing)}"] + it }
+          .join("\n\n")
+
+      in AST::Body(expressions:)
+        expressions
+          .map { format(it, indent:) }
+          .join("\n")
+
+      # Declarations
+
+      in AST::FunctionDeclaration(name:, params:, return_type:, body:)
+        params_str =
+          params
+            .map { format(it) }
+            .join(", ")
+
+        [
+          "def #{name}(#{params_str}) -> #{format_type(return_type)}".then(&and_indent(indent)),
+          format(body, indent: indent + 1),
+          "end".then(&and_indent(indent)),
+        ]
+          .join("\n")
+
+      in AST::FunctionDeclarationParam(name:, type:)
+        "#{name}: #{format_type(type)}"
+
+      in AST::TypeDeclaration(name:, type_params:, variants:)
+        params_str = type_params.empty? ? "" : "(#{type_params.map(&:name).join(', ')})"
+        variants_str =
+          variants
+            .map { format(it) }
+            .join(" | ")
+
+        "type #{name}#{params_str} = #{variants_str}"
+          .then(&and_indent(indent))
+
+      in AST::VariantDeclaration(name:, args:)
+        if args.nil? || args.empty?
+          name
+        else
+          args_str =
+            args
+              .map { format_type(it) }
+              .join(', ')
+
+          "#{name}(#{args_str})"
+        end
+
+      in AST::StructDeclaration(name:, type_params:, record_type:)
+        params_str = type_params.empty? ? "" : "(#{type_params.map(&:name).join(', ')})"
+
+        "struct #{name}#{params_str} = #{format_type(record_type)}"
+          .then(&and_indent(indent))
+
+      in AST::ImportDeclaration(module_name:, as:, exposing:)
+        parts = ["import #{module_name}"]
+        parts << "as #{as.as}" if as
+        parts << format_exposing(exposing) if exposing && !exposing.is_a?(AST::ExposeNone)
+
+        parts
+          .join(' ')
+          .then(&and_indent(indent))
+
+      in AST::InteropImportDeclaration(module: interop_module, functions:)
+        funcs_str =
+          functions
+            .map { format(it).then(&and_indent(indent + 1)) }
+            .join("\n")
+
+        "uses #{interop_module.name} with\n#{funcs_str}"
+          .then(&and_indent(indent))
+
+      in AST::InteropFunction(name:, type:)
+        "#{name} : #{format_type(type)}"
+
+      # Variable binding (let-style)
+
+      in AST::VariableBinding(name:, expression:)
+        "#{name} = #{format(expression)}"
+          .then(&and_indent(indent))
+
+      # Expressions
+
+      in AST::IfThenElse(condition:, if_branch:, else_branch:)
+        [
+          "if #{format(condition)} then".then(&and_indent(indent)),
+          format(if_branch, indent: indent + 1),
+          "else".then(&and_indent(indent)),
+          format(else_branch, indent: indent + 1),
+          "end".then(&and_indent(indent)),
+        ]
+          .join("\n")
+
+      in AST::CaseOf(expression:, branches:)
+        branches_str =
+          branches
+            .map { format(it, indent:) }
+            .join("\n")
+
+        [
+          "case #{format(expression)}".then(&and_indent(indent)),
+          branches_str,
+          "end".then(&and_indent(indent)),
+        ]
+          .join("\n")
+
+      in AST::CaseOfBranch(pattern:, body:)
+        pat_str = format_pattern(pattern)
+
+        if body.expressions.length == 1
+          "of #{pat_str} then #{format(body.expressions.first)}"
+            .then(&and_indent(indent))
+        else
+          [
+            "of #{pat_str} then".then(&and_indent(indent)),
+            format(body, indent: indent + 1),
+          ]
+            .join("\n")
+        end
+
+      in AST::Lambda(params:, body:)
+        params_str =
+          params
+            .map(&:name)
+            .join(', ')
+
+        "(#{params_str}) -> { #{format(body)} }"
+          .then(&and_indent(indent))
+
+      in AST::InfixApplication(left:, operator:, right:)
+        "#{format(left)} #{operator.value} #{format(right)}"
+          .then(&and_indent(indent))
+
+      in AST::FunctionCall(callee:, args:)
+        args_str =
+          args
+            .map { format(it) }
+            .join(', ')
+
+        "#{format(callee)}(#{args_str})"
+          .then(&and_indent(indent))
+
+      in AST::MemberAccess(target:, name:)
+        "#{format(target)}.#{name.name}"
+          .then(&and_indent(indent))
+
+      in AST::QualifiedAccess(target:, name:)
+        "#{format(target)}.#{name}"
+          .then(&and_indent(indent))
+
+      in AST::RecordAccess(target:, name:)
+        "#{format(target)}.#{name.name}"
+          .then(&and_indent(indent))
+
+      in AST::RecordAccessSugar(field_key:)
+        ".#{field_key}"
+          .then(&and_indent(indent))
+
+      in AST::RecordUpdateSugar(field_key:)
+        ".#{field_key} ="
+          .then(&and_indent(indent))
+
+      in AST::Grouping(expression:)
+        "(#{format(expression)})"
+          .then(&and_indent(indent))
+
+      in AST::Tuple(items:)
+        items_str =
+          items
+            .map { format(it) }
+            .join(', ')
+
+        "(#{items_str})"
+          .then(&and_indent(indent))
+
+      in AST::List(items:)
+        items_str =
+          items
+            .map { format(it) }
+            .join(', ')
+
+        "[#{items_str}]"
+          .then(&and_indent(indent))
+
+      in AST::RecordLiteral(fields:)
+        fields_str =
+          fields
+            .map { "#{it.key}: #{format(it.value)}" }
+            .join(', ')
+
+        "{ #{fields_str} }"
+          .then(&and_indent(indent))
+
+      in AST::RecordUpdate(base:, fields:)
+        fields_str =
+          fields
+            .map { "#{it.key}: #{format(it.value)}" }
+            .join(', ')
+
+        "{ #{format(base)} | #{fields_str} }"
+          .then(&and_indent(indent))
+
+      in AST::VariableReference(name:)
+        name
+          .then(&and_indent(indent))
+
+      in AST::ConstructorReference(name:)
+        name
+          .then(&and_indent(indent))
+
+      in AST::Literal(value:)
+        case value
+        in Integer | Float  then value.to_s
+        in TrueClass        then "True"
+        in FalseClass       then "False"
+        in String           then "\"#{value}\""
+        end
+          .then(&and_indent(indent))
+      end
+    end
+
+    private
+
+    def and_indent(indent)
+      ->(str) { "#{INDENT * indent}#{str}" }
+    end
+
+    def format_type(node)
+      case node
+      in AST::TypeName(type:)
+        type
+
+      in AST::QualifiedTypeName(path:)
+        path.join(".")
+
+      in AST::TypeVar(type:)
+        type
+
+      in AST::TypeApplication(constructor:, args:)
+        if args.empty?
+          format_type(constructor)
+        else
+          args_str =
+            args
+              .map { format_type(it) }
+              .join(', ')
+
+          "#{format_type(constructor)}(#{args_str})"
+        end
+
+      in AST::TypeFunction(params:, return_type:)
+        params_str =
+          params
+            .map { format_type(it) }
+            .join(', ')
+
+        "#{params_str} -> #{format_type(return_type)}"
+
+      in AST::TypeRecord(fields:, row_var:)
+        fields_str =
+          fields
+            .map { |k, v| "#{k}: #{format_type(v)}" }
+            .join(", ")
+
+        row_str = row_var ? " | #{row_var}" : ""
+
+        "{ #{fields_str}#{row_str} }"
+
+      in AST::TypeTuple(items:)
+        items_str =
+          items
+            .map { format_type(it) }
+            .join(', ')
+
+        "(#{items_str})"
+      end
+    end
+
+    def format_pattern(node)
+      case node
+      in AST::Pattern::Wildcard
+        "_"
+
+      in AST::Pattern::Literal(literal:)
+        format(literal)
+
+      in AST::Pattern::Binding(name:)
+        name
+
+      in AST::Pattern::Constructor(constructor:, patterns:)
+        name = format(constructor)
+
+        if patterns.nil? || patterns.empty?
+          name
+        else
+          patterns_str =
+            patterns
+              .map { format_pattern(it) }
+              .join(', ')
+
+          "#{name}(#{patterns_str})"
+        end
+
+      in AST::Pattern::Record(fields:)
+        fields_str =
+          fields
+            .map { format_pattern(it) }
+            .join(", ")
+
+        "{ #{fields_str} }"
+
+      in AST::Pattern::RecordField(name:, pattern:)
+        "#{name}: #{format_pattern(pattern)}"
+
+      in AST::Pattern::Tuple(patterns:)
+        patterns_str =
+          patterns
+            .map { format_pattern(it) }
+            .join(', ')
+
+        "(#{patterns_str})"
+      end
+    end
+
+    def format_exposing(node)
+      case node
+      in AST::ExposeAll
+        "exposing (..)"
+
+      in AST::ExposeNone | nil
+        ""
+
+      in AST::ExposeList(items:)
+        items_str =
+          items
+            .map { format_expose_item(it) }
+            .join(', ')
+
+        "exposing (#{items_str})"
+      end
+    end
+
+    def format_expose_item(node)
+      case node
+      in AST::ExposeValue(name:)      then name
+      in AST::ExposeType(name:)       then name
+      in AST::ExposeTypeExpand(name:) then "#{name}(..)"
+      in AST::ExposeAs(as:)           then as
+      end
+    end
+  end
+end
---
+++ spec/lib/jade/formatter_spec.rb
@@ -0,0 +1,604 @@
+require 'spec_helper'
+
+require 'jade/ast'
+require 'jade/parsing'
+require 'jade/lexer'
+require 'jade/formatter'
+
+module Jade
+  describe Formatter do
+    let(:source) { Source.new(uri: 'test', text:) }
+
+    subject do
+      Lexer.tokenize(source)
+        .then { Parsing.parse(it) }
+        .map { Formatter.format(it) } => Ok(result)
+
+      result
+    end
+
+    context 'literals' do
+      context 'int' do
+        let(:text) { "42" }
+        it { is_expected.to eql "42" }
+      end
+
+      context 'float' do
+        let(:text) { "3.14" }
+        it { is_expected.to eql "3.14" }
+      end
+
+      context 'bool True' do
+        let(:text) { "True" }
+        it { is_expected.to eql "True" }
+      end
+
+      context 'bool False' do
+        let(:text) { "False" }
+        it { is_expected.to eql "False" }
+      end
+
+      context 'string' do
+        let(:text) { '"hello"' }
+        it { is_expected.to eql '"hello"' }
+      end
+    end
+
+    context 'variable binding and reference' do
+      let(:text) do
+        <<~JADE.strip
+          x = 42
+          x
+        JADE
+      end
+
+      it { is_expected.to eql "x = 42\nx" }
+    end
+
+    context 'infix application' do
+      let(:text) { "a + b" }
+      it { is_expected.to eql "a + b" }
+
+      context 'nested' do
+        let(:text) { "a + b * c" }
+        it { is_expected.to eql "a + b * c" }
+      end
+
+      context 'comparison' do
+        let(:text) { "x == 0" }
+        it { is_expected.to eql "x == 0" }
+      end
+    end
+
+    context 'grouping' do
+      let(:text) { "(a + b)" }
+      it { is_expected.to eql "(a + b)" }
+    end
+
+    context 'function call' do
+      let(:text) { "f(a, b)" }
+      it { is_expected.to eql "f(a, b)" }
+
+      context 'no args' do
+        let(:text) { "f()" }
+        it { is_expected.to eql "f()" }
+      end
+
+      context 'nested' do
+        let(:text) { "f(g(x), 1)" }
+        it { is_expected.to eql "f(g(x), 1)" }
+      end
+    end
+
+    context 'list' do
+      let(:text) { "[1, 2, 3]" }
+      it { is_expected.to eql "[1, 2, 3]" }
+    end
+
+    context 'tuple' do
+      let(:text) { "(1, 2)" }
+      it { is_expected.to eql "(1, 2)" }
+
+      context 'three elements' do
+        let(:text) { "(1, 2, 3)" }
+        it { is_expected.to eql "(1, 2, 3)" }
+      end
+    end
+
+    context 'record literal' do
+      let(:text) { '{ name: "Alice", age: 30 }' }
+      it { is_expected.to eql '{ name: "Alice", age: 30 }' }
+    end
+
+    context 'record update' do
+      let(:text) { '{ person | name: "Bob" }' }
+      it { is_expected.to eql '{ person | name: "Bob" }' }
+    end
+
+    context 'lambda' do
+      let(:text) { '(x) -> { x + 1 }' }
+      it { is_expected.to eql '(x) -> { x + 1 }' }
+
+      context 'multiple params' do
+        let(:text) { '(x, y) -> { x + y }' }
+        it { is_expected.to eql '(x, y) -> { x + y }' }
+      end
+    end
+
+    context 'if/then/else' do
+      let(:text) do
+        <<~JADE.strip
+          if x == 0 then
+            1
+          else
+            2
+          end
+        JADE
+      end
+
+      it do
+        is_expected.to eql <<~JADE.strip
+          if x == 0 then
+            1
+          else
+            2
+          end
+        JADE
+      end
+
+      context 'nested inside a function body' do
+        let(:text) do
+          <<~JADE.strip
+            def abs(x: Int) -> Int
+              if x < 0 then
+                0 - x
+              else
+                x
+              end
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            def abs(x: Int) -> Int
+              if x < 0 then
+                0 - x
+              else
+                x
+              end
+            end
+          JADE
+        end
+      end
+    end
+
+    context 'case/of' do
+      context 'single-expression branches' do
+        let(:text) do
+          <<~JADE.strip
+            case x
+            of True then 1
+            of False then 0
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            case x
+            of True then 1
+            of False then 0
+            end
+          JADE
+        end
+      end
+
+      context 'wildcard pattern' do
+        let(:text) do
+          <<~JADE.strip
+            case x
+            of _ then 0
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            case x
+            of _ then 0
+            end
+          JADE
+        end
+      end
+
+      context 'constructor with args' do
+        let(:text) do
+          <<~JADE.strip
+            case m
+            of Just(x) then x
+            of Nothing then 0
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            case m
+            of Just(x) then x
+            of Nothing then 0
+            end
+          JADE
+        end
+      end
+
+      context 'tuple pattern' do
+        let(:text) do
+          <<~JADE.strip
+            case pair
+            of (a, b) then a
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            case pair
+            of (a, b) then a
+            end
+          JADE
+        end
+      end
+
+      context 'record pattern' do
+        let(:text) do
+          <<~JADE.strip
+            case p
+            of { x: a, y: b } then a
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            case p
+            of { x: a, y: b } then a
+            end
+          JADE
+        end
+      end
+
+      context 'literal pattern' do
+        let(:text) do
+          <<~JADE.strip
+            case n
+            of 0 then "zero"
+            of _ then "other"
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            case n
+            of 0 then "zero"
+            of _ then "other"
+            end
+          JADE
+        end
+      end
+    end
+
+    context 'function declaration' do
+      context 'single expression body' do
+        let(:text) do
+          <<~JADE.strip
+            def add(a: Int, b: Int) -> Int
+              a + b
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            def add(a: Int, b: Int) -> Int
+              a + b
+            end
+          JADE
+        end
+      end
+
+      context 'multi-expression body' do
+        let(:text) do
+          <<~JADE.strip
+            def compute(x: Int) -> Int
+              y = x + 1
+              y * 2
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            def compute(x: Int) -> Int
+              y = x + 1
+              y * 2
+            end
+          JADE
+        end
+      end
+
+      context 'no params' do
+        let(:text) do
+          <<~JADE.strip
+            def zero() -> Int
+              0
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            def zero() -> Int
+              0
+            end
+          JADE
+        end
+      end
+    end
+
+    context 'type declaration' do
+      context 'simple enum' do
+        let(:text) do
+          <<~JADE.strip
+            module Foo exposing (..)
+
+            type Color = Red | Green | Blue
+          JADE
+        end
+
+        it { is_expected.to include "type Color = Red | Green | Blue" }
+      end
+
+      context 'variant with args' do
+        let(:text) do
+          <<~JADE.strip
+            module Foo exposing (..)
+
+            type Shape = Circle(Int) | Rect(Int, Int)
+          JADE
+        end
+
+        it { is_expected.to include "type Shape = Circle(Int) | Rect(Int, Int)" }
+      end
+
+      context 'with type params' do
+        let(:text) do
+          <<~JADE.strip
+            module Foo exposing (..)
+
+            type Maybe(a) = Just(a) | Nothing
+          JADE
+        end
+
+        it { is_expected.to include "type Maybe(a) = Just(a) | Nothing" }
+      end
+    end
+
+    context 'struct declaration' do
+      context 'simple' do
+        let(:text) do
+          <<~JADE.strip
+            module Foo exposing (..)
+
+            struct Point = { x: Int, y: Int }
+          JADE
+        end
+
+        it { is_expected.to include "struct Point = { x: Int, y: Int }" }
+      end
+
+      context 'with type param' do
+        let(:text) do
+          <<~JADE.strip
+            module Foo exposing (..)
+
+            struct Box(a) = { value: a }
+          JADE
+        end
+
+        it { is_expected.to include "struct Box(a) = { value: a }" }
+      end
+    end
+
+    context 'import declaration' do
+      context 'simple' do
+        let(:text) do
+          <<~JADE.strip
+            module Foo exposing (..)
+
+            import Math.Utils
+          JADE
+        end
+
+        it { is_expected.to include "import Math.Utils" }
+      end
+
+      context 'with alias' do
+        let(:text) do
+          <<~JADE.strip
+            module Foo exposing (..)
+
+            import Math.Utils as M
+          JADE
+        end
+
+        it { is_expected.to include "import Math.Utils as M" }
+      end
+
+      context 'with exposing' do
+        let(:text) do
+          <<~JADE.strip
+            module Foo exposing (..)
+
+            import Math.Utils exposing (add, sub)
+          JADE
+        end
+
+        it { is_expected.to include "import Math.Utils exposing (add, sub)" }
+      end
+    end
+
+    context 'module' do
+      context 'with exposing list' do
+        let(:text) do
+          <<~JADE.strip
+            module Math exposing (add)
+
+            def add(a: Int, b: Int) -> Int
+              a + b
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            module Math exposing (add)
+
+            def add(a: Int, b: Int) -> Int
+              a + b
+            end
+          JADE
+        end
+      end
+
+      context 'with expose all' do
+        let(:text) do
+          <<~JADE.strip
+            module Foo exposing (..)
+
+            def bar() -> Int
+              1
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            module Foo exposing (..)
+
+            def bar() -> Int
+              1
+            end
+          JADE
+        end
+      end
+    end
+
+    context 'type annotations' do
+      context 'type application' do
+        let(:text) do
+          <<~JADE.strip
+            module Foo exposing (..)
+
+            def wrap(x: Int) -> Maybe(Int)
+              Just(x)
+            end
+          JADE
+        end
+
+        it { is_expected.to include "def wrap(x: Int) -> Maybe(Int)" }
+      end
+
+      context 'function type (single param)' do
+        let(:text) do
+          <<~JADE.strip
+            def apply(f: Int -> Int, x: Int) -> Int
+              f(x)
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            def apply(f: Int -> Int, x: Int) -> Int
+              f(x)
+            end
+          JADE
+        end
+      end
+
+      context 'function type (multi param)' do
+        let(:text) do
+          <<~JADE.strip
+            def apply(f: Int, String -> Bool, x: Int) -> Bool
+              f(x, "a")
+            end
+          JADE
+        end
+
+        it { is_expected.to include "f: Int, String -> Bool" }
+      end
+
+      context 'record type' do
+        let(:text) do
+          <<~JADE.strip
+            def get_name(p: { name: String, age: Int }) -> String
+              p.name
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            def get_name(p: { name: String, age: Int }) -> String
+              p.name
+            end
+          JADE
+        end
+      end
+
+      context 'tuple type' do
+        let(:text) do
+          <<~JADE.strip
+            def fst(pair: (Int, String)) -> Int
+              pair
+            end
+          JADE
+        end
+
+        it do
+          is_expected.to eql <<~JADE.strip
+            def fst(pair: (Int, String)) -> Int
+              pair
+            end
+          JADE
+        end
+      end
+    end
+
+    context 'idempotency — formatting twice yields the same result' do
+      let(:text) do
+        <<~JADE.strip
+          module Greeter exposing (greet)
+
+          def greet(name: String) -> String
+            greet_helper(name)
+          end
+        JADE
+      end
+
+      it 'produces stable output' do
+        first_pass = subject
+
+        second_pass =
+          Lexer.tokenize(Source.new(uri: 'test', text: first_pass))
+            .then { Parsing.parse(it) }
+            .map { Formatter.format(it) }
+            .then { it => Ok(r); r }
+
+        expect(second_pass).to eql first_pass
+      end
+    end
+  end
+end
